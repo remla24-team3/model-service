22fa0fafaff6a2047b9b74d50cf9ce6daaac1e85
AlexandruDumitriu <a.dumitriu-1@student.tudelft.nl>
2024-06-27 11:31:53 +0200

    Add swagger

--- src/app.py
+++ src/app.py
@@ -1,31 +1,36 @@
 """ Main application file. """
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flasgger import Swagger
 from lib_ml_group3.load_model import load_model
 
 model = load_model(
     "model/model.keras", "model/encoder.pickle", "model/tokenizer.pickle"
 )
 
-
 app = Flask(__name__)
-
+swagger = Swagger(app)
 
 @app.route("/API/v1.0/predict", methods=["GET", "POST"])
 def predict():
     """
     Endpoint for making predictions.
-
-    This endpoint expects a URL parameter called 'url'
-    which represents the URL of the input data.
-
-    Returns:
-        str: The predicted value.
-
+    ---
+    parameters:
+      - name: url
+        in: query
+        type: string
+        required: true
+        description: The URL of the input data.
+    responses:
+      200:
+        description: The predicted value.
+        schema:
+          type: string
     """
     url = str(request.args["url"])
-    return model.predict([url])[0]
-
+    prediction = model.predict([url])[0]
+    return jsonify(prediction=prediction)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=105, debug=True)
